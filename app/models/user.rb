4a38f10a0c580679d2d9cbf2dfd5ce22d8cff896
Katie Atrops <katrops@gmail.com>
2015-05-02 20:37:59 -0700

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 		too_long: "max val is 50"}
 	validates :email, presence: true, email: true, length: { maximum: 225,
 		too_long: "max val is 225"}
+
+	has_secure_password 
+	validates :password, length: { minimum: 6 }
 end
