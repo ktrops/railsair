170efb197cb9dbdef0c45e94e9ec10dfcc37ef70
Katie Atrops <katrops@gmail.com>
2015-05-04 19:02:33 -0700

    validating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,17 @@
 class UsersController < ApplicationController
 	def new
-		@user = User.new
+	  @user = User.new
 	end
 
 	def create
 		@user = User.new(user_params)
-		# if @user.errors[:email].any?
-		# 	redirect_to root_url, notice: @user.errors.messages
-		# end
-		# if @user.invalid?
-		# 	redirect_to root
+
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to inquiry_path, notice: "You have signed up"
 
 		else
-			redirect_to root_url, notice: "Try again"
+		  render :new, notice: "Try again"
 		end
 	end
 
