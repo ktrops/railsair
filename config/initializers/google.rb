e0d4b2cc91a267869cc9edc96a19a6493cc1ba1b
Katie Atrops <katrops@gmail.com>
2015-05-13 13:54:59 -0700

    to add SSL certs

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,5 +1,26 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, GOOGLE[:google_client_id], GOOGLE[:google_client_secret], provider_ignores_state: true
+options = {
+  :name => "google",
+  :scope => "email, profile",
+  :prompt => "select_account",
+  :image_aspect_ratio => "square",
+  :image_size => 50
+}
+
+if !Rails.env.production?
+  options.merge!({
+    :client_options => {
+      :ssl => {
+        :ca_file => "/usr/lib/ssl/certs/ca-bundle.crt",
+        :ca_path => "/usr/lib/ssl/certs"
+      }
+    }
+  })
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, GOOGLE[:google_client_id], GOOGLE[:google_client_secret], provider_ignores_state: true, options
+    # {:client_options => {:ssl => {:ca_path => "/usr/lib/ssl/certs"}}}
+end
+
